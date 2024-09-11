03b1b8aaa6e407a8a7123796c9552022768eb06a
fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>
2024-09-11 16:47:16 +0800

    Signed-off-by: fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>

---
+++ src/components/demo01.vue
@@ -0,0 +1,22 @@
+<template>
+    <!--html-->
+    <div class="boxl">
+        hhh...
+    </div>
+    
+</template>
+
+<script setup>
+   // js
+   //alert("Hello");
+
+</script>
+
+<style scoped>
+    /*css*/
+    .boxl{
+        color: rgb(152, 152, 223);
+        font-size: 50px;
+    }
+
+</style>
